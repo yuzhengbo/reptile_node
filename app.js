bd2fa4aa9ed1bd62a626ee39f050cf97f4eac474
yuzhengbo <newobject@foxmail.com>
2018-05-24 14:56:24 +0800

    1

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-var http = require('http');
-// var fs = require('fs');
-// var hostName = 'localhost';
-// var port = 3000;
-// var express = require('express');
-// var app = express();
-// // 启动服务
-// var server = http.createServer(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('hello nodejs');
-// })
-// server.listen(port, hostName, function () {
-//   console.log(`服务器运行在http://${hostName}:${port}`);
-// })
-// // 路由
-// app.get('/', function (req, res) {
-//   res.send('hello word')
-// })
-
+var proxy = require('express-http-proxy');
 var express = require('express');
+var xpath = require('xpath');
+var parse5 = require('parse5');
+var xmlser = require('xmlserializer');
+var dom = require('xmldom').DOMParser
 var app = express();
 app.get('/', function (req, res) {
   res.send('hello world');
@@ -28,20 +14,11 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 })
 
-app.get('/reptile', function (req, res) {
-  var url = 'http://www.lizhi.fm/';
-  function fetchPage (url) {
-    startRequest(url)
-  };
-  function startRequest (url) {
-    http.get(url, function (res) {
-      console.log(url)
-      var html = '';
-      var titles = [];
-      res.setEncoding('utf-8');
-      console.log(res)
-    })
-  };
-  fetchPage(url)
-  res.send('成功')
-})
+var url = 'http://www.lizhi.fm/';
+app.use('/proxy', proxy(url, {
+  userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
+    data = proxyResData.toString('utf8');
+    // return data;
+    return "node使用xpath爬取html会出现问题"
+  }
+}))

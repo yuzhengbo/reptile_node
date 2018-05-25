4c420039a2a52386ba6b2867d8a9d7377dfa4171
yuzhengbo <newobject@foxmail.com>
2018-05-25 19:13:48 +0800

    11

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var proxy = require('express-http-proxy');
 var express = require('express');
 var xpath = require('xpath');
-var parse5 = require('parse5');
-var xmlser = require('xmlserializer');
 var dom = require('xmldom').DOMParser
+var cheerio = require('cheerio');
 var app = express();
 app.get('/', function (req, res) {
   res.send('hello world');
@@ -18,10 +17,17 @@ var url = 'http://www.lizhi.fm/';
 app.use('/proxy', proxy(url, {
   userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
     xml = proxyResData.toString('utf8');
-    var doc = new dom().parseFromString(xml) 
-    var nodes = xpath.select('//*[@id="allRadioTag"]', doc)
-    data = nodes
-    return data.toString();
+    const $ = cheerio.load(xml);
+    var category = {
+      name: $('#allRadioTag', 'a').text()
+    }
+    console.log(category);
+    data = category;
+    return data
+    // var doc = new dom().parseFromString(xml) 
+    // var nodes = xpath.select('//*[@id="allRadioTag"]', doc)
+    // data = nodes
+    // return data.toString();
     // return "node使用xpath爬取html会出现问题"
   }
 }))
